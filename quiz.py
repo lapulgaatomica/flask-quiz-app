f32e4aaf419cfb20ab7dab43d0d5bd1df5c1d3cd
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2020-08-22 14:18:12 +0100

    Modified config again

--- quiz.py
+++ quiz.py
@@ -1,6 +1,6 @@
 import os
 import click
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 from app import create_app, db
 from app.models import User, Course, Question, Result
 from flask import g
